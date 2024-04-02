4ec3aa4dc3087269af3acb273035e5adc1444a35
Thaingx <90052906+ThaiNgxBa@users.noreply.github.com>
2024-04-03 01:14:26 +0700

    Implement UI

---
+++ FurtherProgramming-Assignment1/src/Assignment1/ClaimManagementSystem/UI/Claim/ClaimPage.java
@@ -0,0 +1,46 @@
+package UI.Claim;
+
+import UI.MainPage;
+
+import java.util.Scanner;
+
+public class ClaimPage {
+
+    public static void run() {
+        displayOptions();
+    }
+
+    private static void displayOptions() {
+        System.out.println("What do you want to do with claims");
+        System.out.println("1. Add Claim");
+        System.out.println("2. Update Claim");
+        System.out.println("3. Delete Claim");
+        System.out.println("0. Go Back");
+        System.out.print("Enter your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        switch (choice) {
+            case "1":
+//                AddClaim.run();
+                break;
+            case "2":
+//                UpdateClaim.run();
+                break;
+            case "3":
+//                DeleteClaim.run();
+                break;
+            case "0":
+//                MainPage.run();
+                break;
+            default:
+                System.out.println();
+                System.out.println("⚠️ Invalid choice. Please select a valid option.");
+                displayOptions();
+                break;
+        }
+    }
+
+
+}
--- FurtherProgramming-Assignment1/src/Assignment1/ClaimManagementSystem/Utility/Main.java
+++ FurtherProgramming-Assignment1/src/Assignment1/ClaimManagementSystem/Utility/Main.java
@@ -1,7 +1,9 @@
 package Utility;
 
+import UI.MainPage;
+
 public class Main {
     public static void main(String[] args) {
-
+        MainPage.run();
     }
 }
